2a572fa781796e9693a2dfd6221ce91fa4912ce0
Park Ju Hyung <qkrwngud825@gmail.com>
2016-10-01 20:32:14 +0900

    Add build.sh
    And use this instead.
    autoconf in this project is fucked up.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+/home/arter97/android/ndk/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-g++ \
+    -DHAVE_CONFIG_H -I. -I./src -DHAVE_CONFIG_H -D_FILE_OFFSET_BITS=64 -Wall \
+    -O3 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fPIC -fPIE -pie -pipe -s \
+    -o AtomicParsley \
+       src/util.cpp \
+       src/arrays.cpp \
+       src/iconv.cpp \
+       src/parsley.cpp \
+       src/extracts.cpp \
+       src/sha1.cpp \
+       src/uuid.cpp \
+       src/id3v2.cpp \
+       src/metalist.cpp \
+       src/CDtoc.cpp \
+       src/compress.cpp \
+       src/main.cpp \
+       src/errno.cpp \
+       src/splice.S \
+    -lz
