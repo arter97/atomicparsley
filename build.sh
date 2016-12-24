3206dcf46672de1b36b4d679a44b321530b11f96
Park Ju Hyung <qkrwngud825@gmail.com>
2016-12-24 23:15:22 +0900

    Tune for cortex-a15.cortex-a7
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 /home/arter97/android/ndk/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-g++ \
     -DHAVE_CONFIG_H -I. -I./src -DHAVE_CONFIG_H -D_FILE_OFFSET_BITS=64 -Wall \
-    -O3 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fPIC -fPIE -pie -pipe -s \
+    -O3 -march=armv7-a -mtune=cortex-a15.cortex-a7 -mfloat-abi=softfp -mfpu=neon -fPIC -fPIE -pie -pipe -s \
     -o AtomicParsley \
        src/util.cpp \
        src/arrays.cpp \
